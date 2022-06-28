1fa962bf0bdb4b2ada58d56dfca47acc192e013f
Teemu Lahin <tlahin@dabian.dabian>
2022-06-28 10:07:56 -0400

    moved all sourcefiles and headers to root of the libft folder

--- libft/libft.h
+++ libft/libft.h
@@ -63,7 +63,6 @@ char	*ft_strsub(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s);
 char	**ft_strsplit(char const *s, char c);
-char	*ft_itoa(int n);
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char const *s, int fd);
 void	ft_putendl_fd(char const *s, int fd);
