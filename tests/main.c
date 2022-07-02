79d38761b3164614f1d7b58e550f1ef4b2301d4c
Teemu Lahin <tlahin@c3r1p7.hive.fi>
2022-07-02 19:47:58 +0300

    added folder for tests and created a simple main.c

---
+++ tests/main.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+#include "../includes/ft_printf.h"
+
+int main(void)
+{
+	char c;
+	char v;
+
+	c = 'c';
+	v = '%';
+	ft_printf("Moi: \n");
+	ft_printf("Moi: %%\n");
+	return (1);
+}
