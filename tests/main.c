3424e5bfc86fff20a4d2945883dbceccc99f5a09
Teemu Lahin <tlahin@c3r2p2.hive.fi>
2022-07-03 21:17:07 +0300

    More tests added

--- tests/main.c
+++ tests/main.c
@@ -6,14 +6,24 @@ int main(void)
 {
 	char	c;
 	char	*str;
+	int		i;
+	char	*ptr;
 
 	c = 'A';
 	str = "ABC";
+	i = 42;
+	ptr = str;
 	ft_printf("\nft_Test: Hello %%!\n");
 	printf("Test   : Hello %%!\n\n");
 	ft_printf("ft_Test: %c\n", c);
 	printf("Test   : %c\n\n", c);
 	ft_printf("ft_Test: %s\n", str);
 	printf("Test   : %s\n\n", str);
+	ft_printf("ft_Test: %d\n", i);
+	printf("Test   : %d\n\n", i);
+	ft_printf("ft_Test: %i\n", i);
+	printf("Test   : %i\n\n", i);
+	ft_printf("ft_Test: %p\n", &ptr);
+	printf("Test   : %p\n\n", &ptr);
 	return (1);
 }
