70760d52c5ad83c14cd5057efdea78c70afebdcb
Teemu Lahin <tlahin@c3r2p2.hive.fi>
2022-07-03 20:28:18 +0300

    More simple tests

--- tests/main.c
+++ tests/main.c
@@ -4,12 +4,16 @@
 
 int main(void)
 {
-	char c;
-	char v;
+	char	c;
+	char	*str;
 
-	c = 'c';
-	v = '%';
-	ft_printf("Moi: \n");
-	ft_printf("Moi: %%\n");
+	c = 'A';
+	str = "ABC";
+	ft_printf("\nft_Test: Hello %%!\n");
+	printf("Test   : Hello %%!\n\n");
+	ft_printf("ft_Test: %c\n", c);
+	printf("Test   : %c\n\n", c);
+	ft_printf("ft_Test: %s\n", str);
+	printf("Test   : %s\n\n", str);
 	return (1);
 }
