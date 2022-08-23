e07cfe706f627048eac260a22a08fda32c25c5ae
Teemu Lahin <tlahin@dabian.dabian>
2022-08-23 10:26:16 -0400

    more tests cases added

--- tests/main.c
+++ tests/main.c
@@ -19,12 +19,16 @@ int	main(void)
 	int		i;
 	char	*ptr1;
 	char	*ptr2;
+	int	x;
+	int	u;
 
 	c = 'A';
 	str = "ABC";
 	i = 123;
 	ptr1 = str;
 	ptr2 = NULL;
+	x = 42;
+	u = 3000000;
 
 	printf("\n%%\n");
 	ft_printf("ft_printf: Hello %%!\n");
@@ -52,6 +56,22 @@ int	main(void)
 	
 	printf("pointer null\n");
 	ft_printf("ft_printf: %p\n", &ptr2);
-	printf("OG_printf: %p\n", &ptr2); 
+	printf("OG_printf: %p\n\n", &ptr2); 
+
+	printf("octal decimal\n");
+	ft_printf("ft_printf: %o\n", x);
+	printf("OG_printf: %o\n\n", x);
+
+	printf("unsigned integer\n");
+	ft_printf("ft_printf: %u\n", u);
+	printf("OG_printf: %u\n\n", u);
+
+	printf("smoll hexa decimal\n");
+	ft_printf("ft_printf: %x\n", x);
+	printf("OG_printf: %x\n\n", x);
+
+	printf("big hexa decimal\n");
+	ft_printf("ft_printf: %X\n", x);
+	printf("OG_printf: %X\n\n", x);
 	return (1);
 }
