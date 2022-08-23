cbceddd2b1a4fae0e9e8d7ed317db924e80eaa1a
Teemu Lahin <tlahin@dabian.dabian>
2022-08-23 10:25:59 -0400

    uint typos fixed, and functions added

--- sources/print_uint.c
+++ sources/print_uint.c
@@ -2,36 +2,50 @@
 
 #include "ft_printf.h"
 
+int	ft_num_len(unsigned int num)
+{
+	int	len;
+
+	len = 0;
+	while (num != 0)
+	{
+		len++;
+		num = num / 10;
+	}
+	return (len);
+}
+
 char	*ft_uitoa(unsigned int num)
 {
-	char	*num;
+	char	*str;
 	int	len;
 
 	len = ft_num_len(num);
-	number = (char *)malloc(sizeof(char) * 	(len + 1));
-	if (!num)
-		return (-1);
-	num[len] = '\0';
+	str = (char *)malloc(sizeof(char) * (len + 1));
+	if (!str)
+		return (0);
+	str[len] = '\0';
 	while (num != 0)
 	{
-		num[len - 1] = n % 10 + 48;
-		n = n / 10;
+		str[len - 1] = num % 10 + 48;
+		num = num / 10;
 		len--;
 	}
-	return (num);
+	return (str);
 }
 
 int	ft_print_uint(unsigned int num)
 {
 	int	print_length;
+	char	*str;
 
 	print_length = 0;
 	if (num == 0)
 		print_length += write(1, "0", 1);
 	else
 	{
-		num = ft_uitoa(num);
-		print_length += ft_printstr(num);
+		str = ft_uitoa(num);
+		print_length += ft_print_str(str);
 	}	
 	return (print_length);
 }
