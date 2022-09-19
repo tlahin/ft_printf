61d4999e5829e39307e1a75dfa4e1bf6e58201dd
Teemu Lahin <tlahin@c2r1p12.hive.fi>
2022-09-19 14:53:21 +0300

    new tests that need to be passed

--- main.c
+++ main.c
@@ -4,56 +4,25 @@
 
 int	main(void)
 {
-	//int	real;
+	int	real;
 	int mine;
 
+	real = printf("%5u", 65535);
+	printf("\n");
+	mine = ft_printf("%5u", 65535);
+	printf("\n");
+	printf("OG_len: %d\n", real);
+	printf("FT_len: %d\n", mine);
+	printf("\n");
 	/*
+	real = printf("%15u", 4294967295);
 	printf("\n");
-	real = printf("@moulitest: %#.o %#.0o", 0, 0);
-	printf("\n");
-	mine = ft_printf("@moulitest: %#.o %#.0o", 0, 0);
+	mine = ft_printf("%15u", 4294967295);
 	printf("\n");
 	printf("real_len: %d\n", real);
 	printf("mine_len: %d\n", mine);
 	printf("\n");
-	*/
-	/*
-	real = printf("%d %d", 1, -2);
-	printf("\n");
-	mine = ft_printf("%d %d", 1, -2);
-	printf("\n");
-	printf("real_len: %d\n", real);
-	printf("mine_len: %d\n", mine);
-	printf("\n");
-	*/
-	/*
-	real = printf("%10s is a string", "this");
-	printf("\n");
-	mine = ft_printf("%10s is a string", "this");
-	printf("\n");
-	printf("real_len: %d\n", real);
-	printf("mine_len: %d\n", mine);
-	printf("\n");
-	*/
-	/*
-	real = printf("%5.2s is a string", "this"); 
-	printf("\n");
-	mine = ft_printf("%5.2s is a string", "this"); 
-	printf("\n");
-	printf("real_len: %d\n", real);
-	printf("mine_len: %d\n", mine);
-	printf("\n");
-	*/
-	/*
-	real = printf("%5u", 4294967295);
-	printf("\n");
-	mine = ft_printf("%5u", 4294967295); 
-	printf("\n");
-	printf("real_len: %d\n", real);
-	printf("mine_len: %d\n", mine);
-	printf("\n");
-	*/
-	/*
+
 	real = printf("%015u", 4294967295);
 	printf("\n");
 	mine = ft_printf("%015u", 4294967295);
@@ -61,38 +30,10 @@ int	main(void)
 	printf("real_len: %d\n", real);
 	printf("mine_len: %d\n", mine);
 	printf("\n");
-	*/
-	/*
-	real = printf("%d %d %d %d gg!", 1, -2, 33, 42, 0);
-	printf("\n");
-	mine = ft_printf("%d %d %d %d gg!", 1, -2, 33, 42, 0);
-	printf("\n");
-	printf("real_len: %d\n", real);
-	printf("mine_len: %d\n", mine);
-	printf("\n");
-	*/
-	/*
+
 	real = printf("%lu", -42);
 	printf("\n");
 	mine = ft_printf("%lu", -42);
-	printf("\n");
-	printf("real: %d\n", real);
-	printf("mine: %d\n", mine);
-	printf("\n");
-	*/
-	
-	//real = printf("%");
-	printf("here\n");
-	mine = ft_printf("%");
-	printf("\n");
-	//printf("real_len: %d\n", real);
-	printf("mine_len: %d\n", mine);
-	printf("\n");
-	
-	/*
-	real = printf("%5+d", 42);
-	printf("\n");
-	mine = ft_printf("%5+d", 42);
 	printf("\n");
 	printf("real_len: %d\n", real);
 	printf("mine_len: %d\n", mine);
