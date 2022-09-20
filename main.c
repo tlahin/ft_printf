11d24d7a245792d31b97f5cd7352046c456a9b99
Teemu Lahin <tlahin@c1r5p17.hive.fi>
2022-09-20 18:20:47 +0300

    new test

--- main.c
+++ main.c
@@ -4,26 +4,16 @@
 
 int	main(void)
 {
-	//int	real;
+	int	real;
 	int mine;
 
 	printf("\n-----------\n");
-	printf("%%lu -42\n");
-	//real = printf("%lu", -42);
+	printf("%%05%%\n");
+	real = printf("%05%");
 	printf("\n");
-	mine = ft_printf("%lu", -42);
-	printf("\n\n");
-	//printf("OG_len: %d\n", real);
-	printf("FT_len: %d\n", mine);
-
-	printf("\n-----------\n");
-
-	printf("%% hZ\n");
-	//real = printf("% hZ");
+	mine = ft_printf("%05%");
 	printf("\n");
-	mine = ft_printf("% hZ");
-	printf("\n\n");
-	//printf("OG_len: %d\n", real);
+	printf("OG_len: %d\n", real);
 	printf("FT_len: %d\n", mine);
 
 	printf("\n-----------\n");
