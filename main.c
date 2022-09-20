4ad414eee93329aeddd8e7e3c34ada4f2a8561ba
tlahin <teemu.lahin@gmail.com>
2022-09-20 21:08:48 +0300

    added new main tests

--- main.c
+++ main.c
@@ -7,18 +7,27 @@ int	main(void)
 	int	real;
 	int mine;
 
-	printf("\n-----------\n");
+/* 	printf("-----------\n\n");
 	printf("lu, lo, lx, lX and %% with max_values aka 0/\n\n");
-	//real = printf("%lx\n%lX\n%lo\n%lu\n%%", "0/", "/0", "/0", "/0", "0/");
-	real = printf("%#10.lx", (unsigned long)0);
+	real = printf("%lx\n%lX\n%lo\n%lu\n%%", "0/", "/0", "/0", "/0", "0/");
 	printf("\n");
-	//mine = ft_printf("%lx\n%lX\n%lo\n%lu\n%%", "0/", "/0", "/0", "/0", "0/");
-	mine = ft_printf("%#10.lx", (unsigned long)0);
+	mine = ft_printf("%lx\n%lX\n%lo\n%lu\n%%", "0/", "/0", "/0", "/0", "0/");
+	printf("\n");
+	printf("OG_len: %d\n", real);
+	printf("FT_len: %d\n", mine); */
+
+	printf("\n-----------\n");
+	
+	printf("%%-10.10%%\n\n");
+	real = printf("%-10.10%");
+	printf("\n");
+	mine = ft_printf("%-10.10%");
 	printf("\n");
 	printf("OG_len: %d\n", real);
 	printf("FT_len: %d\n", mine);
 
 	printf("\n-----------\n");
+
 
 	return (0);
 }
