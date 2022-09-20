c7350483204749fbebcc88b7aa09e04c1a0724d8
Teemu Lahin <tlahin@c2r1p12.hive.fi>
2022-09-20 12:13:18 +0300

    New tests, removed old passed ones

--- main.c
+++ main.c
@@ -7,37 +7,12 @@ int	main(void)
 	int	real;
 	int mine;
 
-	real = printf("%5u", 4294967295);
-	printf("\n");
-	mine = ft_printf("%5u", 4294967295);
-	printf("\n");
-	printf("OG_len: %d\n", real);
-	printf("FT_len: %d\n", mine);
-	printf("\n");
-	/*
-	real = printf("%15u", 4294967295);
-	printf("\n");
-	mine = ft_printf("%15u", 4294967295);
-	printf("\n");
-	printf("real_len: %d\n", real);
-	printf("mine_len: %d\n", mine);
-	printf("\n");
-
-	real = printf("%015u", 4294967295);
-	printf("\n");
-	mine = ft_printf("%015u", 4294967295);
-	printf("\n");
-	printf("real_len: %d\n", real);
-	printf("mine_len: %d\n", mine);
-	printf("\n");
-
 	real = printf("%lu", -42);
 	printf("\n");
 	mine = ft_printf("%lu", -42);
 	printf("\n");
-	printf("real_len: %d\n", real);
-	printf("mine_len: %d\n", mine);
+	printf("OG_len: %d\n", real);
+	printf("FT_len: %d\n", mine);
 	printf("\n");
-	*/
 	return (0);
 }
