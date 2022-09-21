b3f9463cd5822814b004a67c3bfeb00c87e1ba69
Teemu Lahin <tlahin@c2r1p12.hive.fi>
2022-09-21 14:54:59 +0300

    new tests for double

--- main.c
+++ main.c
@@ -7,17 +7,65 @@ int main(void)
 	int real;
 	int mine;
 
-	printf("\n-----------\n");
-	printf("lu, lo, lx, lX and %%\n\n");
+/* 	printf("\n---------------------\n\n");
+	printf("%%*.*d, 0, 3, 0\n\n");
 	printf("OG_printf: \n");
-	real = printf("%lx\n%lX\n%lo\n%lu\n%%", ULONG_MAX, ULONG_MAX, ULONG_MAX, ULONG_MAX, ULONG_MAX);
+	real = printf("%*.*d", 0, 3, 0);
 	printf("\n\n");
 	printf("FT_printf: \n");
-	mine = ft_printf("%lx\n%lX\n%lo\n%lu\n%%", ULONG_MAX, ULONG_MAX, ULONG_MAX, ULONG_MAX, ULONG_MAX);
+	mine = ft_printf("%*.*d", 0, 3, 0);
 	printf("\n\n");
 	printf("OG_printf returns: %d\n", real);
 	printf("FT_printf returns: %d\n", mine);
-	printf("\n-----------\n");
 
+	printf("\n---------------------\n\n");
+
+	printf("{%%*3d}, 5, 0\n\n");
+	printf("OG_printf: \n");
+	real = printf("{%*3d}", 5, 0);
+	printf("\n\n");
+	printf("FT_printf: \n");
+	mine = ft_printf("{%*3d}", 5, 0);
+	printf("\n\n");
+	printf("OG_printf returns: %d\n", real);
+	printf("FT_printf returns: %d\n", mine);
+
+	printf("\n---------------------\n\n");
+
+	printf("%%*d, 5, 42\n\n");
+	printf("OG_printf: \n");
+	real = printf("%*d", 5, 42);
+	printf("\n\n");
+	printf("FT_printf: \n");
+	mine = ft_printf("%*d", 5, 42);
+	printf("\n\n");
+	printf("OG_printf returns: %d\n", real);
+	printf("FT_printf returns: %d\n", mine);
+
+	printf("\n---------------------\n\n");
+
+	printf("%%f%%lf%%Lf, 1.42, 1.42, 1.42l\n\n");
+	printf("OG_printf: \n");
+	real = printf("%f\n%lf\n%Lf", 1.42, 1.42, 1.42l);
+	printf("\n\n");
+	printf("FT_printf: \n");
+	mine = ft_printf("%f\n%lf\n%Lf", 1.42, 1.42, 1.42l);
+	printf("\n\n");
+	printf("OG_printf returns: %d\n", real);
+	printf("FT_printf returns: %d\n", mine);*/
+
+	printf("\n---------------------\n\n");
+
+	printf("%%f, 1.42\n\n");
+	printf("OG_printf: \n");
+	real = printf("%f\n", 1.42);
+	printf("\n\n");
+	printf("FT_printf: \n");
+	mine = ft_printf("%f\n", 1.42);
+	printf("\n\n");
+	printf("OG_printf returns: %d\n", real);
+	printf("FT_printf returns: %d\n", mine);
+
+	printf("\n---------------------\n\n");
 	return (0);
 }
