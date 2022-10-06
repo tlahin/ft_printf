621946d9a8a7663c693a53a9abfba5a1e20f9fd1
Teemu Lahin <tlahin@c3r1p7.hive.fi>
2022-10-06 08:17:41 +0300

    changed write to print alernative, coulndt figure out line 33 print alternative so using write

--- sources/print_float.c
+++ sources/print_float.c
@@ -30,11 +30,11 @@ static void	put_f(t_data *info, long double number, int not_empty, \
 		print_alternative(info, ' ', info->width - not_empty);
 	if (info->prefix[2] == ' ' && number >= 0 && \
 			info->prefix[1] != '+')
-		write (1, " ", 1);
+		write(1, " ", 1);
 	if (number < 0)
 		print_alternative(info, '-', 1);
 	if (number >= 0 && info->prefix[1] == '+')
-		write (1, "+", 1);
+		print_alternative(info, '+', info->width - not_empty);
 	if (info->prefix[4] == '0' && zero >= 1)
 		print_alternative(info, '0', zero);
 }
